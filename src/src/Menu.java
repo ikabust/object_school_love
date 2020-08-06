d58bcd3e1ba6ed19fc541f7eb96cb5a3484061be
Miyawaki0bkr <kiyo_retoruto@yahoo.co.jp>
2020-08-06 20:29:05 +0900

    screen change

--- src/src/Menu.java
+++ src/src/Menu.java
@@ -18,58 +18,58 @@ import javax.swing.JPanel;
  */
 @SuppressWarnings("serial")
 public class Menu extends JPanel{
-		
+
 	public Menu(){
-		
+
 		super(new GridBagLayout());
-		
+
 		GridBagConstraints c = new GridBagConstraints();
-        
+
        JLabel instructions = new JLabel("Welcome");
        Myfont.setFontSize(instructions, 30);
        c.insets = new Insets(10,10,10,10);
        c.gridy = 0;
        this.add(instructions, c);
-       
+
        Button startButton = new Button("New Game");
        startButton.setName("startButton");
        c.gridy = 2;
        this.add(startButton, c);
-       
+       /*
        Button optionButton = new Button("Options");
        optionButton.setName("optionsButton");
        c.gridy = 3;
        this.add(optionButton, c);
-       
+       */
        Button rulesButton = new Button("Rules");
        rulesButton.setName("rulesButton");
        c.gridy = 4;
        this.add(rulesButton, c);
-      
+
        Frame frame = Frame.getFrame();
        startButton.addActionListener(frame);
-       optionButton.addActionListener(frame);
+       // optionButton.addActionListener(frame);
        rulesButton.addActionListener(frame);
-       
+
        this.setBackground(new Color(0,0,0,20));
 	}
-	
+
 	/*
 	 * This method is used to paint the background image of the menu
 	 * The image change according to the theme
 	 */
 	@Override
 	protected void paintComponent(Graphics g){
-		
+
 		super.paintComponent(g);
-		ImageIcon ii = new ImageIcon(getClass().getResource("/" + Frame.getTheme() + "_back.png"));
+		ImageIcon ii = new ImageIcon(getClass().getResource("/surprise15.png"));
         Image image = ii.getImage();
 		g.drawImage(image, 0, 0, null);
-		
+
 		//a transparent white square is drawn on the background image to make it transparent
 		g.setColor(new Color(255,255,255,200)); //the fourth parameter, on a scale from 0 to 255, defines the opacity
 		g.fillRect(0, 0, 500, 500);
-		
+
 	}
 
 }
--- src/src/Score.java
+++ src/src/Score.java
@@ -7,29 +7,29 @@ import java.io.Serializable;
  */
 @SuppressWarnings("serial")
 public class Score implements Serializable{
-	
+
 	private String playerName; //name of the player (Anonymous by default)
 	private int score; //score at the end of the game
-	
+
 	public Score(String playerName, int score){
-		
+
 		this.playerName = playerName;
 		this.score = score;
-		
+
 	}
-	
+
 	/*
 	 * This String will be used and shown at the end of the game by the ScoreBoard class
 	 */
 	public String toString(){
-		
-		return playerName + " with " + score + " points \n";
+
+		return score + " seconds \n";
 	}
-	
+
 	public int getScore(){
 		return score;
 	}
-	
+
 	public String getPlayerName(){
 		return playerName;
 	}
